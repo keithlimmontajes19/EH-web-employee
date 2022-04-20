b806a52279c48fb049dc46403d0b6d32c28149e6
keithlimmontajes <keithlimmontajes@gmail.com>
2022-04-21 01:36:22 +0800

    [feature] employee web

--- src/compositions/QuizSort/QuizSort.tsx
+++ src/compositions/QuizSort/QuizSort.tsx
@@ -1,10 +1,65 @@
-import {ReactElement} from 'react';
+import {ReactElement, useState} from 'react';
 
 import type {PropsType} from './types';
-import {} from './styled';
+import {Container} from './styled';
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd';
+import IconImage from 'components/IconImage';
+import SORT_ICON from 'assets/icons/drag-icon.png';
 
-const QuizSort = (props: PropsType): ReactElement => {
-  return <>4</>;
+const QuizSort = ({item}: PropsType): ReactElement => {
+  const [SORT_LIST, SET_SORT_LIST] = useState(item?.resource?.answer || []);
+
+  const onDragEnd = (result) => {
+    const {destination, source, draggableId} = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.draggableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const newObjects = Array.from(SORT_LIST);
+
+    newObjects.splice(source.index, 1);
+    newObjects.splice(destination.index, 0, draggableId);
+
+    SET_SORT_LIST(newObjects);
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      {(SORT_LIST || []).map((choice, index) => {
+        return (
+          <Droppable droppableId={index.toString()}>
+            {(provided) => (
+              <div
+                ref={provided.innerRef}
+                innerRef={provided.innerRef}
+                {...provided.droppableProps}>
+                <Draggable draggableId={choice.toString()} index={index}>
+                  {(provided1) => (
+                    <div
+                      ref={provided1.innerRef}
+                      innerRef={provided1.innerRef}
+                      {...provided1.draggableProps}
+                      {...provided1.dragHandleProps}>
+                      <Container>
+                        <IconImage source={SORT_ICON} width={50} height={60} />
+                        &nbsp;&nbsp;
+                        {choice}
+                      </Container>
+                    </div>
+                  )}
+                </Draggable>
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        );
+      })}
+    </DragDropContext>
+  );
 };
 
 export default QuizSort;
