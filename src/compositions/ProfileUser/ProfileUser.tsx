c473bd486dc70fa3d31b10135d53fd08e2bd7d61
akiracadet <mavis.vermillion.ichi@gmail.com>
2022-07-31 20:02:41 +0800

    completed update profile

--- src/compositions/ProfileUser/ProfileUser.tsx
+++ src/compositions/ProfileUser/ProfileUser.tsx
@@ -1,46 +1,80 @@
-import axios from 'axios';
-import Gleap from 'gleap';
-
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import axios from 'axios'
+import {useCallback} from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useDispatch} from 'react-redux'
 
 import {
-  StyledSave,
+  reloadUser,
+  useGetSingleUserQuery,
+  useUpdateUserAvatarMutation,
+  useUpdateUserProfileMutation
+} from 'ducks/users/usersApiSlice'
+
+import {
   StyledText,
-  StyledLabel,
-  StyledInput,
-  StyledCancel,
   RowContainer,
   FlexContainer,
-  ButtonContainer,
   UploadContainer,
-} from './styled';
-import {Avatar, Spin, message } from 'antd';
-import UploadButton from 'components/UploadButton';
-
-import { imageTypes } from 'utils/file-types';
-import USER_LOGO from 'assets/icons/profile-user.png';
-import { reloadUser, useGetSingleUserQuery, useUpdateUserAvatarMutation } from 'ducks/users/usersApiSlice';
+} from './styled'
 
 import styles from './ProfileUser.module.css'
+import USER_LOGO from 'assets/icons/profile-user.png'
+import {Avatar, Spin, message} from 'antd'
+import {imageTypes} from 'utils/file-types'
 
 export default function ProfileUser() {
   const dispatch = useDispatch()
   const navigate = useNavigate()
 
   const userId = localStorage.getItem('userId')
+  if (!userId) navigate('/logout')
 
-  if (!userId) navigate('/login')
+  const {data: user} = useGetSingleUserQuery(userId)
+  const [updateUserAvatar, {isLoading: isLoadingUpdateUserAvatar}] = useUpdateUserAvatarMutation()
+  const [updateUserProfile, {isLoading: isLoadingUpdateUserProfile}] = useUpdateUserProfileMutation()
 
-  const { data: user } = useGetSingleUserQuery(userId)
-  const [updateUserAvatar, { isLoading }] = useUpdateUserAvatarMutation()
+  const onFormProfileSubmit = useCallback((event) => {
+    event.preventDefault()
+    // const profile = Object.fromEntries(new FormData(event.target))
+
+    const profile = Array
+      .from(new FormData(event.target))
+      .filter(([, v]) => v)
+      .reduce((current, value) => ({...current, [value[0]]: value[1]}), {})
+
+    updateUserProfile({userId, profile})
+      .then(() => {
+        event.target.reset()
+      })
+      .catch(() => {
+        message.error('Error in updating user profile.')
+      })
+  }, [])
+
+  const onAvatarChange = useCallback((event) => {
+    const file = event.target.files[0]
+
+    if (!file) return
+    if (!imageTypes.includes(file.type)) {
+      message.error('Unsupported file type.')
+      return
+    }
+
+    updateUserAvatar(userId)
+      .then((result: any) => {
+        const url = result?.data?.updateUrl
+
+        axios
+          .put(url, file, { headers: { 'Content-Type': '' } })
+          .then(() => dispatch(reloadUser(userId)))
+      })
+  }, [])
 
   return <>
     <RowContainer>
       <FlexContainer>
         <StyledText>My Profile</StyledText>
-        <Spin spinning={isLoading}>
+        <Spin spinning={isLoadingUpdateUserAvatar}>
           <Avatar size={140} src={user?.profile?.avatar || USER_LOGO} />
         </Spin>
         <UploadContainer>
@@ -48,46 +82,49 @@ export default function ProfileUser() {
             id="user-profile-avatar-upload"
             type="file"
             style={{ display: 'none' }}
-            onChange={(event) => {
-              const file = event.target.files[0]
-
-              if (!file) return
-              if (!imageTypes.includes(file.type)) {
-                message.error('Unsupported file type.')
-                return
-              }
-
-              updateUserAvatar(userId)
-                .then((result: any) => {
-                  const url = result?.data?.updateUrl
-
-                  axios
-                    .put(url, file, { headers: { 'Content-Type': '' } })
-                    .then(() => dispatch(reloadUser(userId)))
-                })
-            }}
+            onChange={onAvatarChange}
           />
           <input
             type="button"
             className={styles['upload-button']}
             value="Change Photo"
-            onClick={() => { document.getElementById('user-profile-avatar-upload').click() }}
+            onClick={() => {document.getElementById('user-profile-avatar-upload').click()}}
           />
         </UploadContainer>
 
-        <StyledLabel>First Name</StyledLabel>
-        <StyledInput />
+        <form className={styles['form-profile']} onSubmit={onFormProfileSubmit}>
+          <label htmlFor="firstName" className={styles['form-profile__label']}>First Name</label>
+          <input
+            id="firstName"
+            className={styles['form-profile__input']}
+            type="text"
+            name="firstName"
+            placeholder={user?.profile?.firstName}
+          />
 
-        <StyledLabel>Last Name</StyledLabel>
-        <StyledInput />
+          <label htmlFor="lastName" className={styles['form-profile__label']}>Last Name</label>
+          <input
+            id="lastName"
+            className={styles['form-profile__input']}
+            type="text"
+            name="lastName"
+            placeholder={user?.profile?.lastName}
+          />
 
-        <StyledLabel style={{ paddingLeft: 25 }}>Phone Number </StyledLabel>
-        <StyledInput />
+          <label htmlFor="phoneNumber" className={styles['form-profile__label']}>Phone Number</label>
+          <input
+            id="phoneNumber"
+            className={styles['form-profile__input']}
+            type="text"
+            name="phoneNumber"
+            placeholder={user?.profile?.phoneNumber}
+          />
 
-        <ButtonContainer>
-          <StyledCancel>CANCEL</StyledCancel>
-          <StyledSave>SAVE</StyledSave>
-        </ButtonContainer>
+          <div className={styles['form-profile__button-container']}>
+            <button className={styles['form-profile__button-cancel']} type="reset">CANCEL</button>
+            <button className={styles['form-profile__button-save']} type="submit">SAVE <Spin spinning={isLoadingUpdateUserProfile}></Spin></button>
+          </div>
+        </form>
       </FlexContainer>
     </RowContainer>
   </>
