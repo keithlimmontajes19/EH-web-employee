1d8bdd450e5a6e8e737e192dfa12bff156396770
akiracadet <mavis.vermillion.ichi@gmail.com>
2022-08-01 16:15:51 +0800

    partial implementation new feature

--- src/views/private/Home/HomeInfo/HomeInfo.tsx
+++ src/views/private/Home/HomeInfo/HomeInfo.tsx
@@ -1,35 +1,11 @@
-import {useState, useRef} from 'react'
 import {faUser, faBuilding, faGraduationCap, faVideo, faPollH, faFile} from '@fortawesome/free-solid-svg-icons'
 import styles from './HomeInfo.module.css'
 import HomeInfoItem from './HomeInfoItem/HomeInfoItem'
 
 export default function HomeInfo() {
-  const itemsRef: any = useRef()
-  const [clientX, setClientX] = useState(0)
-  const [isScrolling, setIsScrolling] = useState(false)
-
-  const onMouseDown = (e) => {
-    setIsScrolling(true)
-    setClientX(e.clientX)
-  }
-
-  const onMouseUp = () => {
-    setIsScrolling(false)
-  }
-
-  const onMouseMove = (e) => {
-    if (isScrolling) {
-      itemsRef.current.scrollLeft += clientX - e.clientX
-    }
-  }
 
   return <div className={styles['home-info']}>
-    <ul className={styles['home-info__items']}
-      ref={itemsRef}
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
-      onMouseMove={onMouseMove}
-    >
+    <ul className={styles['home-info__items']}>
       <HomeInfoItem icon={faUser} title="Employees" count="50" />
       <HomeInfoItem icon={faBuilding} title="Organizations" count="10" />
       <HomeInfoItem icon={faGraduationCap} title="Courses" count="45" />
