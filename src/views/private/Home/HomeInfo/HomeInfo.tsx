2820ea93827b12bf4857435ec19b766f139b7371
akiracadet <mavis.vermillion.ichi@gmail.com>
2022-08-01 16:54:07 +0800

    implemented scrolling

--- src/views/private/Home/HomeInfo/HomeInfo.tsx
+++ src/views/private/Home/HomeInfo/HomeInfo.tsx
@@ -1,11 +1,57 @@
+import {useEffect} from 'react'
 import {faUser, faBuilding, faGraduationCap, faVideo, faPollH, faFile} from '@fortawesome/free-solid-svg-icons'
 import styles from './HomeInfo.module.css'
 import HomeInfoItem from './HomeInfoItem/HomeInfoItem'
 
 export default function HomeInfo() {
+  useEffect(() => {
+    document.addEventListener('DOMContentLoaded', function () {
+      const ele = document.getElementById('info-items');
+      ele.style.cursor = 'grab';
+
+      let pos = { top: 0, left: 0, x: 0, y: 0 };
+
+      const mouseDownHandler = function (e) {
+        ele.style.cursor = 'grabbing';
+        ele.style.userSelect = 'none';
+
+        pos = {
+          left: ele.scrollLeft,
+          top: ele.scrollTop,
+          // Get the current mouse position
+          x: e.clientX,
+          y: e.clientY,
+        };
+
+        document.addEventListener('mousemove', mouseMoveHandler);
+        document.addEventListener('mouseup', mouseUpHandler);
+      };
+
+      const mouseMoveHandler = function (e) {
+        // How far the mouse has been moved
+        const dx = e.clientX - pos.x;
+        const dy = e.clientY - pos.y;
+
+        // Scroll the element
+        ele.scrollTop = pos.top - dy;
+        ele.scrollLeft = pos.left - dx;
+      };
+
+      const mouseUpHandler = function () {
+        ele.style.cursor = 'grab';
+        ele.style.removeProperty('user-select');
+
+        document.removeEventListener('mousemove', mouseMoveHandler);
+        document.removeEventListener('mouseup', mouseUpHandler);
+      };
+
+      // Attach the handler
+      ele.addEventListener('mousedown', mouseDownHandler);
+    });
+  }, [])
 
   return <div className={styles['home-info']}>
-    <ul className={styles['home-info__items']}>
+    <ul id="info-items" className={styles['home-info__items']}>
       <HomeInfoItem icon={faUser} title="Employees" count="50" />
       <HomeInfoItem icon={faBuilding} title="Organizations" count="10" />
       <HomeInfoItem icon={faGraduationCap} title="Courses" count="45" />
