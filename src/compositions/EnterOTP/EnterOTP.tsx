a94b4705d56b06a30d95d595c25392cf2ea6f5a8
vdelossantos001 <von@noblemen.com>
2022-08-21 01:22:26 +0800

    implemented enter otp

--- src/compositions/EnterOTP/EnterOTP.tsx
+++ src/compositions/EnterOTP/EnterOTP.tsx
@@ -1,12 +1,32 @@
 import {useEffect, useState} from 'react'
 import {Link, useNavigate} from 'react-router-dom'
+import {useMutation} from 'react-query'
 import {AuthLayout} from 'layouts/AuthLayout'
 import logo from 'assets/icons/logo.png'
 import styles from './EnterOTP.module.css'
+import {verifyOTP} from 'api/otpAPI'
 
 export function EnterOTP() {
   const [otpEmail, setOTPEmail] = useState('')
+  const [otpCode, setOTPCode] = useState(null)
   const navigate = useNavigate()
+
+  const verifyOTPMutation = useMutation(verifyOTP, {
+    onSuccess: (data: any) => {
+      const {accessToken, refreshToken, userId} = data
+      localStorage.setItem('accessToken', accessToken)
+      localStorage.setItem('refreshToken', refreshToken)
+      localStorage.setItem('userId', userId)
+      navigate('../reset')
+    },
+    onError: (err: any) => {
+      if (err.response) {
+        return err.response.data.message
+      }
+
+      return err.message
+    }
+  })
 
   useEffect(() => {
     const otpEmail = localStorage.getItem('otpEmail')
@@ -20,8 +40,26 @@ export function EnterOTP() {
       <h1 className={`${styles.title}`}>Enter OTP</h1>
       <p className={`${styles.instruction}`}>A 4 digit code has been sent to {`${otpEmail}`}</p>
       <form className={`${styles.form}`}  onSubmit={(e) => e.preventDefault()}>
-        <input className={`${styles.textInput}`} type="text" placeholder="Enter OTP" />
-        <button className={`${styles.submitButton}`} type="submit">SUBMIT</button>
+        <input
+          className={`${styles.textInput}`}
+          type="text"
+          placeholder="Enter OTP"
+          value={otpCode}
+          onChange={(e) => {
+            setOTPCode(Number(e.target.value) || null)
+          }}
+        />
+        <button
+          className={`${styles.submitButton}`}
+          type="submit"
+          onClick={async () => {
+            if (otpCode && otpCode > 999) {
+              await verifyOTPMutation.mutateAsync({email: otpEmail, code: otpCode})
+            }
+          }}
+        >
+          SUBMIT
+        </button>
         <Link to="/login" className={styles.cancel}>CANCEL</Link>
       </form>
     </div>
