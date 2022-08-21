8b504bfc9afa8966ccacb8d75b0afb50c0b36b97
vdelossantos001 <von@noblemen.com>
2022-08-22 01:29:26 +0800

    manually added url for api

--- src/api/authAPI.ts
+++ src/api/authAPI.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const baseURL = process.env.REACT_APP_BASE_API_URL || 'https://engage-hub-platform-dev.herokuapp.com/api/v1'
+// const baseURL = process.env.REACT_APP_BASE_API_URL || 'https://engage-hub-platform-dev.herokuapp.com/api/v1'
+const baseURL = 'https://engage-hub-platform-dev.herokuapp.com/api/v1'
 
 export const authAPI = axios.create({
   baseURL: `${baseURL}/auth`,
