f1a58260052a3798fc0b53252161ad9aff3e6311
vdelossantos001 <von@noblemen.com>
2022-10-02 19:24:12 +0800

    implemented multiple choice

--- src/compositions/Question/MultipleChoice/MultipleChoice.tsx
+++ src/compositions/Question/MultipleChoice/MultipleChoice.tsx
@@ -1,7 +1,26 @@
+import {useState} from 'react'
 import styles from './MultipleChoice.module.css'
 
 export default function MultipleChoice(props) {
   const {question} = props
+  const {answer, choices} = question
+  const [userAnswer, setUserAnswer] = useState([])
 
-  return <div></div>
+  return <ul className={`${styles.choices}`}>
+    {choices.map((choice, index) => {
+      return <li
+        key={index}
+        className={`${styles.choice} ${userAnswer.includes(choice) && styles.selected}`}
+        onClick={() => {
+          if (userAnswer.includes(choice)) {
+            setUserAnswer((prev) => prev.filter((val) => val !== choice))
+          } else {
+            setUserAnswer((prev) => [...prev, choice])
+          }
+        }}
+      >
+        <p>{choice}</p>
+      </li>
+    })}
+  </ul>
 }
--- src/compositions/Question/Question.tsx
+++ src/compositions/Question/Question.tsx
@@ -11,12 +11,12 @@ export default function Question(props) {
   const {isLoading, question} = useGetSingleQuestion(questions[questionPOS]._id)
 
   return <div className={`${styles.questionContainer}`}>
-    <h3 className={`${styles.quizTitle}`}>{quiz.title}</h3>
-    <h4 className={`${styles.questionTitle}`}>{question.title}</h4>
-    {!isLoading && question.questionType === 'single-choice' && <SingleChoice question={question} />}
-    {!isLoading && question.questionType === 'multiple-choice' && <MultipleChoice question={question} />}
-    {!isLoading && question.questionType === 'essay' && <Essay question={question} />}
-    {!isLoading && question.questionType === 'fill-in-the-blanks' && <FillInTheBlanks question={question} />}
-    {!isLoading && question.questionType === 'sort' && <Sort question={question} />}
+    <h3 className={`${styles.quizTitle}`}>{quiz?.title}</h3>
+    <h4 className={`${styles.questionTitle}`}>{question?.title}</h4>
+    {!isLoading && question?.questionType === 'single-choice' && <SingleChoice question={question} />}
+    {!isLoading && question?.questionType === 'multiple-choice' && <MultipleChoice question={question} />}
+    {!isLoading && question?.questionType === 'essay' && <Essay question={question} />}
+    {!isLoading && question?.questionType === 'fill-in-the-blanks' && <FillInTheBlanks question={question} />}
+    {!isLoading && question?.questionType === 'sort' && <Sort question={question} />}
   </div>
 }
