afd72021b7f2985f2b7ba1809a24f7be95d83ed6
vdelossantos001 <von@noblemen.com>
2022-10-03 16:05:45 +0800

    implemented single choice

--- src/compositions/Question/SingleChoice/SingleChoice.tsx
+++ src/compositions/Question/SingleChoice/SingleChoice.tsx
@@ -1,7 +1,29 @@
-export default function SingleChoice(props) {
-  const {question} = props
+import {useGetSingleQuestionAnswer} from 'api/questionsAPI'
+import {useEffect} from 'react'
+import styles from './SingleChoice.module.css'
 
-  return <div>
-    <p>SingleChoice</p>
-  </div>
+export default function SingleChoice(props) {
+  const {question, userAnswer, setUserAnswer} = props
+  const {answer, choices} = question
+
+  const {isLoading, isError, error, questionAnswer, tag}: any = useGetSingleQuestionAnswer(question._id)
+
+  useEffect(() => {
+    if (!isLoading && !isError) setUserAnswer(questionAnswer)
+    else setUserAnswer([])
+  }, [questionAnswer])
+
+  return (!isLoading && <ul className={`${styles.choices}`}>
+    {choices.map((choice, index) => {
+      return <li
+        key={index}
+        className={`${styles.choice} ${userAnswer === choice && styles.selected}`}
+        onClick={() => {
+          setUserAnswer(choice)
+        }}
+      >
+        <p>{choice}</p>
+      </li>
+    })}
+  </ul>)
 }
