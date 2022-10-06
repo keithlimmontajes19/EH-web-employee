f09945533a2eeadde40704e540bdaef365ec0ec9
vdelossantos001 <von@noblemen.com>
2022-10-06 19:30:01 +0800

    partial implementation of sorting

--- src/compositions/Question/Sort/Sort.tsx
+++ src/compositions/Question/Sort/Sort.tsx
@@ -1,7 +1,22 @@
+import {useEffect} from 'react'
+import {useGetSingleQuestionAnswer} from 'api/questionsAPI'
 import styles from './Sort.module.css'
 
 export default function Sort(props) {
-  const {question} = props
+  const {question, userAnswer, setUserAnswer} = props
+  const {isLoading, isError, error, questionAnswer, tag}: any = useGetSingleQuestionAnswer(question._id)
+
+  useEffect(() => {
+    if (!isLoading && !isError) {
+      if (!areEqual(questionAnswer, question.answer)) setUserAnswer(() => shuffle(question.answer))
+      else setUserAnswer(() => questionAnswer)
+    }
+  }, [isLoading])
+
+  if (userAnswer === null) return <></>
 
   return <></>
 }
+
+function areEqual(a, b) {return JSON.stringify(a.sort()) === JSON.stringify(b.sort())}
+function shuffle(arr) {return arr.map(value => ({ value, sort: Math.random() })).sort((a, b) => a.sort - b.sort).map(({ value }) => value)}
